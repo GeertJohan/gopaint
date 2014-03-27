e49200150737c1a6ddbbf46741b247a9b8b4ef28
Geert-Johan Riemer <geertjohan.riemer@gmail.com>
2014-03-27 10:44:31 +0100

    Remove tester from repo. Add it to .gitignore

--- tester/main.go
+++ tester/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	for x := 0; x < 40; x++ {
 		for y := 0; y < 40; y++ {
-			resp, err := http.Get(fmt.Sprintf("http://localhost:1234/draw?x=%d&y=%d&color=0000ff&layer=test3", x, y))
+			resp, err := http.Get(fmt.Sprintf("http://localhost:1234/draw?x=%d&y=%d&color=0f7733&layer=leon", x, y))
 			if err != nil {
 				log.Fatalf("error drawing with http.Get: %s\n", err)
 			}
